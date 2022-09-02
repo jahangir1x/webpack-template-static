a6f224393f4091596bffaef1ef4ad08ca37e13fd
Md. Jahangir Alam Rocky <x00jahangir@gmail.com>
2022-09-03 01:35:47 +0600

    Cache busting and improvements. - html-loader added - Production cache busting added - asset modules added to load files

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,6 +7,7 @@ module.exports = merge(common, {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[name][ext]"
   },
   devtool: 'inline-source-map',
   devServer: {
